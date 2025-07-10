98387c9302810c6900314575558edf9cfc52406a
Prathap Koththigoda <51660157+prathap-koththigoda@users.noreply.github.com>
2025-07-10 13:10:39 +0200

    ICT edition 2025

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Software Entwickler", "Web Designer", "Fotograf", "IT Student"],
+        strings: ["Software Entwickler", "System Engineer", "Web Designer", "Fotograf", "IT Student"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Software Entwickler", "Web Designer", "Fotograf", "IT Student"],
+        strings: ["Software Entwickler", "System Engineer", "Web Designer", "Fotograf", "IT Student"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
